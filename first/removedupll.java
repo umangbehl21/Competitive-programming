6590a58361bad1aad10b35ed5d5a7021f7105df1
Umang Behl <umangbehl2002@gmail.com>
2022-02-01 22:23:07 +0530

    work

---
+++ first/removedupll.java
@@ -0,0 +1,30 @@
+package first;
+
+public class removedupll {
+    public Node<Integer> deleteDuplicates(Node<Integer> head) {
+        if(head == null || head.next == null)
+        {
+            return head;
+        }
+        Node<Integer> ans = head;
+        Node<Integer> temp = head;
+        
+        while(temp != null && temp.next != null)
+        {
+            while(temp.next != null && temp.data == temp.next.data)
+            {
+                temp = temp.next;
+            }
+            ans.next = temp.next;
+            ans = ans.next;
+            temp = temp.next;
+        }
+            return head;
+            
+            
+        }
+
+
+
+
+}
