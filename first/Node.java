eb37b99c0234638d7803062e8fd3a484215e12e8
Umang Behl <umangbehl2002@gmail.com>
2022-02-01 17:17:57 +0530

    work

---
+++ first/Node.java
@@ -0,0 +1,13 @@
+package first;
+
+    public class Node<t>{
+        t data;
+        Node<t> next; //reference to the next node in the link list
+        Node(t data )
+        {
+            this.data = data;
+            next = null;
+        }
+    }
+    
+
