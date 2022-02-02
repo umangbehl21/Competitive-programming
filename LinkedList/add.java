c6f3b4624172a6b7262c0b3cf9918c283699f252
Umang Behl <umangbehl2002@gmail.com>
2022-02-02 13:56:53 +0530

    removeduplicates

---
+++ LinkedList/add.java
@@ -0,0 +1,8 @@
+package LinkedList;
+
+public class add {
+    public static void main(String[] args) {
+        int a = 5,b=6;
+        System.out.println(a+b);
+    }
+}
---
+++ LinkedList/checkcycleinll.java
@@ -0,0 +1,18 @@
+package LinkedList;
+
+public class checkcycleinll {
+    public boolean hasCycle(Node<Integer> head) {
+        Node<Integer> slow = head;
+        Node<Integer> fast = head;
+        while(fast != null && fast.next != null)
+        {
+         slow = slow.next;
+         fast = fast.next.next;
+         if(slow == fast)
+         {
+            return true;
+         }
+        }
+            return false; 
+        }
+}
