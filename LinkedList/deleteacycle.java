83de541b1d69dfddcab31550df98c6e2645c47a3
Umang Behl <umangbehl2002@gmail.com>
2022-02-05 20:23:10 +0530

    LinkedList

--- LinkedList/deleteacycle.java
+++ LinkedList/deleteacycle.java
@@ -21,18 +21,18 @@ public class deleteacycle {
 
        
         if (slow == fast) {
-            slow = head;
-              if (slow != fast) {
-                  while (slow.next != fast.next) {
+            slow = head; //point slow on head again 
+              if (slow != fast) { //now slow will be on head and fast will be near looping point so they are not equal
+                  while (slow.next != fast.next) {   //one by one steping them forward until there next is same
                       slow = slow.next;
                       fast = fast.next;
                   }
               
-               fast.next = null; 
+               fast.next = null;  //breaking the loop
             }
              
             else {  //this one will only run when tail is pointing to head 
-                while(fast.next != slow) {
+                while(fast.next != slow) {  //when fast and slow are together on first position we can increase fast until its next becomes slow then we can break its link 
                     fast = fast.next;
                 }
                 fast.next = null;
