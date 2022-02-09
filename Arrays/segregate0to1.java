5a207deac5627a884809afa0715bea90c307f8cf
Umang Behl <umangbehl2002@gmail.com>
2022-02-09 19:24:25 +0530

    Arrays

--- Arrays/segregate0to1.java
+++ Arrays/segregate0to1.java
@@ -29,7 +29,7 @@ public class segregate0to1 {
          segregate0and1(arr);
          for(int i = 0;i<arr.length;i++)
          {
-             System.out.println(arr[i]+" ");
+             System.out.print(arr[i]+" ");
          }
            
     }
