8a6b9a224bd7e26c78f89abab6cbe319075ab3ba
Umang Behl <umangbehl2002@gmail.com>
2022-02-09 19:30:03 +0530

    Arrays

--- Arrays/segregate0to1.java
+++ Arrays/segregate0to1.java
@@ -1,7 +1,7 @@
 package Arrays;
 
 public class segregate0to1 {
-   public static void segregate0and1(int[] arr) {
+    public static void segregate0and1(int[] arr) {
         int i = 0;
         int j = 0;
         while (i < arr.length) {
@@ -18,19 +18,19 @@ public class segregate0to1 {
         }
     }
 
-  public static void swap(int arr[], int i, int j) {
+    public static void swap(int arr[], int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
 
     }
+
     public static void main(String[] args) {
-        int arr[] = {1,1,0,1,0,1,1,0,1,0,0,0,1};
-         segregate0and1(arr);
-         for(int i = 0;i<arr.length;i++)
-         {
-             System.out.print(arr[i]+" ");
-         }
-           
+        int arr[] = { 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1 };
+        segregate0and1(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
     }
 }
