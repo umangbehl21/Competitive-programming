5a684ffb54a7b7eada4e072eef325aa829a0495d
Umang Behl <umangbehl2002@gmail.com>
2022-03-02 18:50:43 +0530

    sum of array recursion

--- recursion/sumofarray.java
+++ recursion/sumofarray.java
@@ -1,19 +1,15 @@
 package recursion;
 
 public class sumofarray {
-    public static int sum(int arr[]) {
-        if (arr.length <= 1) {
-            return arr[0];
+    public static int sum(int arr[], int n) {
+        if (n <= 0) {
+            return 0;
         }
-        int smallarr[] = new int[arr.length - 1];
-        for (int i = 1; i < arr.length; i++) {
-            smallarr[i - 1] = arr[i];
-        }
-        return sum(smallarr) + arr[0];
+        return sum(arr, n - 1) + arr[n - 1];
     }
 
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5,6,7};
-        System.out.println(sum(arr));
+        int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+        System.out.println(sum(arr, arr.length));
     }
 }
