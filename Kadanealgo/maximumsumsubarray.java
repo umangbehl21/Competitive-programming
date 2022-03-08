4ffa1dc363be5a30143ee445872fe4b1ca7919e2
Umang Behl <umangbehl2002@gmail.com>
2022-03-08 19:36:40 +0530

    maximum sum subarray

--- Kadanealgo/maximumsumsubarray.java
+++ Kadanealgo/maximumsumsubarray.java
@@ -3,8 +3,8 @@ package Kadanealgo;
 public class maximumsumsubarray {
     public static int maxsum(int arr[])
     {
-       int csum = arr[0];
-       int osum = arr[0];
+       int csum = 0;
+       int osum = 0;
        for(int i = 0;i<arr.length;i++)
        {
            if(csum >= 0)
@@ -24,7 +24,7 @@ public class maximumsumsubarray {
 
     }
     public static void main(String[] args) {
-        int arr[] = {-2,1,-3,4,-1,2,1,-5,4};
+        int arr[] = {1 ,2 ,3 ,-2,5};
         int a = maxsum(arr);
         System.out.println(a);
     }
