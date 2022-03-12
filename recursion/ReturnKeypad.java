5bebc30ab6fc35630ae467a57f748a3a4f26a959
Umang Behl <umangbehl2002@gmail.com>
2022-03-12 16:18:41 +0530

    Return keypad revise

--- recursion/ReturnKeypad.java
+++ recursion/ReturnKeypad.java
@@ -17,7 +17,7 @@ public static ArrayList<String> keypad(String str) //"678"
     ArrayList<String> smallans = keypad(ncall); 
     ArrayList<String> fres = new ArrayList<>(); //making arraylist which will store final result
        String codesval = codes[ch - '0']; // now we have a global array name as codes we will fetch the string from an index if ch = '6' then first we have to convert character into integer the ascii value of '0' is 48 and '6' is 54 so we have to deduct '6' - '0' (54-48 = 6) which will give us 6 in integer form
-       for(int i = 0;i < codesval.length();i++) //now we will iterate over "678"                                  
+       for(int i = 0;i < codesval.length();i++) //now we will iterate over "pqrs"                                  
        {
            char chcode = codesval.charAt(i); //chcode = 'p'
            for(String res : smallans) //smallans the ArrayList we have recieved from answer of smaller part we will iterate over all the values stored in smallans arraylist
