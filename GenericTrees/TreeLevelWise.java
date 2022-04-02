648746a5c1b6545645b74693a21b89664b53e62e
Umang Behl <umangbehl2002@gmail.com>
2022-04-02 21:40:48 +0530

    Levelwise print,input ,height kth level nodes etc

--- GenericTrees/TreeLevelWise.java
+++ GenericTrees/TreeLevelWise.java
@@ -25,6 +25,22 @@ public class TreeLevelWise {   //functions are implemented with help of queue as
         }
         return root;
     }
+    public static void print(TreeNode<Integer> root)
+    {
+        Queue<TreeNode<Integer>> pendingnodes = new LinkedList<>(); 
+        pendingnodes.add(root);   //add root in queue 
+        while(!pendingnodes.isEmpty())
+        {
+            TreeNode<Integer> frontnode = pendingnodes.remove();   //dequeue it 
+            String s = frontnode.data + ":";  //make string appending root data which is dequeued in frontnode 
+            for(int i = 0;i<frontnode.children.size();i++)
+             {
+                s += frontnode.children.get(i).data + ","; //if root have child its child data will be appended in tue String 
+                pendingnodes.add(frontnode.children.get(i)); //dont forgot to add the root's child into queue because they also have child to be printed
+             }
+             System.out.println(s); //in the end after appending all children string will be printed here 
+        }
+    }
     public static int CountNodes(TreeNode<Integer> root)
     {
         if(root == null) //this is not a basecase this is an edge case our for loop is handling the base case as if there are no children of a node it will return count without entering for loop 
@@ -62,37 +78,139 @@ public class TreeLevelWise {   //functions are implemented with help of queue as
         {
             return 0;
         }
-        if(root.children == null)
+        if(root.children.size() == 0)  //if node have no children return 1 as height of single node is one 
         {
             return 1;
         }
         int ans = 0;
-        for(int i = 0;i<root.children.size();i++)
+        for(int i = 0;i<root.children.size();i++)  //height is greater between left subtree and right subtree + 1 
         {
-            ans = Math.max(ans,HeightofTree(root.children.get(i)));
+            ans = Math.max(ans,HeightofTree(root.children.get(i)));  
         }
         return ans + 1;
     }
-    public static void print(TreeNode<Integer> root)
+    public static void NodesAtKdepth(TreeNode<Integer> root,int k)
     {
-        Queue<TreeNode<Integer>> pendingnodes = new LinkedList<>(); 
-        pendingnodes.add(root);   //add root in queue 
-        while(!pendingnodes.isEmpty())
+       if(k < 0)
+       {
+           return;
+       }
+       if(k == 0)
+       {
+           System.out.println(root.data);
+           return;
+       }
+       for(int i = 0;i<root.children.size();i++)
+       {
+           NodesAtKdepth(root.children.get(i), k-1);
+       }
+
+    }
+    public static int CountLeafNodes(TreeNode<Integer> root)
+    {
+        if(root == null)
         {
-            TreeNode<Integer> frontnode = pendingnodes.remove();   //dequeue it 
-            String s = frontnode.data + ":";  //make string appending root data which is dequeued in frontnode 
-            for(int i = 0;i<frontnode.children.size();i++)
-             {
-                s += frontnode.children.get(i).data + ","; //if root have child its child data will be appended in tue String 
-                pendingnodes.add(frontnode.children.get(i)); //dont forgot to add the root's child into queue because they also have child to be printed
-             }
-             System.out.println(s); //in the end after appending all children string will be printed here 
+            return 0;
         }
+        if(root.children.size() == 0)  //if a node have no children its ArrayList size will be zero and a node having no child is leaf node 
+        {
+            return 1;
+        }
+        int count = 0;
+        for(int i = 0;i<root.children.size();i++)
+        {
+            count += CountLeafNodes(root.children.get(i));
+        }
+        return count;
+    }
+    public static void preorder(TreeNode<Integer> root)
+    {
+        if(root == null) //this is edge case not base case
+        {
+            return;
+        }
+        System.out.println(root.data);
+        for(int i = 0;i<root.children.size();i++)
+        {
+            preorder(root.children.get(i));
+        }
+    }
+    public static void postorder(TreeNode<Integer> root)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        for(int i = 0;i<root.children.size();i++)
+        {
+           postorder(root.children.get(i));
+        }
+        System.out.println(root.data);
+        
+    }
+    public static int greater(TreeNode<Integer> root,int x)
+    {
+        if(root == null)
+        {
+            return 0;
+        }
+        int count = 0;
+        if(root.data > x)
+        {
+            count++;
+        }
+        for(int i = 0;i<root.children.size();i++)
+        {
+            count += greater(root.children.get(i), x);
+        }
+        return count; 
+    }
+    public static int SumOfNodes(TreeNode<Integer> root)
+    {
+        if(root == null)
+        {
+            return 0;
+        }
+        int sum = root.data;
+        for(int i = 0;i<root.children.size();i++)
+        {
+            sum += SumOfNodes(root.children.get(i));
+        }
+        return sum;
+    }
+    public static boolean iscontain(TreeNode<Integer> root,int num)
+    {
+       if(root.data == num)
+       {
+           return true;
+       }
+       for(int i = 0;i<root.children.size();i++)
+       {
+          boolean check = iscontain(root.children.get(i), num); //here we cannot blindly return the smallans because it may happen we find our node data same as num very deeper in tree so we cannot return false seeing the data of one child 
+          if(check == true) //so if smallans is true then only we will return true otherwise after the end of for loop parent function will return false itself
+          {
+              return true;
+          }
+       }
+         return false;
     }
     public static void main(String[] args) {
         TreeNode<Integer> root = inputlevelwise();
         print(root);
         System.out.println("number of nodes are "+CountNodes(root));
+        System.out.println("largest node is "+ largest(root));
+        System.out.println("height of tree is "+ HeightofTree(root));
+        System.out.println("Nodes at Kth depth are ");
+        NodesAtKdepth(root, 2);
+        System.out.println("leaf nodes are "+CountLeafNodes(root));
+        System.out.println("postorder is");
+        postorder(root);
+        System.out.println("preorder is");
+        preorder(root);
+        System.out.println("number of elements greater than x "+greater(root, 2));
+        System.out.println("sum of nodes are "+SumOfNodes(root));
+        System.out.println("iscotain "+iscontain(root, 7));
+
     }
 
 }
