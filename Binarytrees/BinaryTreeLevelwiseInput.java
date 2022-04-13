2c60dea503d131552f31773c0d55c28ec18404c4
Umang Behl <umangbehl2002@gmail.com>
2022-04-13 22:44:54 +0530

    Binary trees folder questions

--- Binarytrees/BinaryTreeLevelwiseInput.java
+++ Binarytrees/BinaryTreeLevelwiseInput.java
@@ -144,9 +144,9 @@ public class BinaryTreeLevelwiseInput {
     }
     public static BinaryTreeNode<Integer> buildtreehelper(int in[],int pre[])
     {
-       return buildtree(in,pre,0,in.length-1,0,pre.length-1);
+       return buildtreePreorder(in,pre,0,in.length-1,0,pre.length-1);
     }
-    private static BinaryTreeNode<Integer> buildtree(int in[],int pre[],int inS,int inE,int preS,int preE)
+    private static BinaryTreeNode<Integer> buildtreePreorder(int in[],int pre[],int inS,int inE,int preS,int preE)  //same explanation as build tree from postorder and inorder below this code
     {
        if(inS > inE)
        {
@@ -171,8 +171,8 @@ public class BinaryTreeLevelwiseInput {
        int riE = inE;
        int rpS = lpe + 1;
        int rpE = preE;
-       root.left = buildtree(in, pre, liS, liE, lpS, lpe);
-       root.right = buildtree(in, pre, ris, riE, rpS, rpE);
+       root.left = buildtreePreorder(in, pre, liS, liE, lpS, lpe);
+       root.right = buildtreePreorder(in, pre, ris, riE, rpS, rpE);
        return root;
         
     }
@@ -181,13 +181,13 @@ public class BinaryTreeLevelwiseInput {
         return BuildTreeFromPostOrderhelp(in, po,0,in.length-1,0,po.length-1);
     }
     private static BinaryTreeNode<Integer> BuildTreeFromPostOrderhelp(int in[],int po[],int inS,int inE,int poS,int poE)
-    { 
-        if(inS > inE)
-        {
+    {                  
+        if(inS > inE) //for left inorder when single element make call then that single element is also our root and left inorder end according to formula is rootindex - 1 so it becomes -1 and our left inorder start is on 0 so 0 > -1 this is the condition 
+        {             //for right inorder when single element remain that is our root also and right inorder start according to formula is rootindex + 1 so it becomes greater than the end so again condition satisfied
             return null;
         }
         int rootdata = po[poE];
-        BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootdata);
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootdata);  //making root node as it will attach left and right element 
         int rootindex = -1;
         for(int i = inS;i<=inE;i++)  //when third recursive call is made inorder start become equal to inorder end so we will write <= in condition 
         {
@@ -201,7 +201,7 @@ public class BinaryTreeLevelwiseInput {
         int leftinE = rootindex - 1;
         int leftpoS = poS;
         int leftpoE = leftinE - leftins + leftpoS;
-        int rightinS = rootindex + 1;
+        int rightinS = rootindex + 1; 
         int rightinE = inE;
         int rightpoS = leftpoE + 1;
         int rightpoE = poE - 1;
@@ -244,27 +244,88 @@ public class BinaryTreeLevelwiseInput {
         return false;
 
     }
+    public static int SumofNodes(BinaryTreeNode<Integer> root)
+    {
+        if(root == null)
+        {
+            return 0;
+        }
+        int sum = 1;
+        sum += SumofNodes(root.left);
+        sum+= SumofNodes(root.right);
+        return sum;
+
+    }
+    public static BinaryTreeNode<Integer> RemoveLeafNodes(BinaryTreeNode<Integer> root)
+    {
+        if(root == null)
+        {
+            return null;
+        }
+        if(root.left == null && root.right == null)
+        {
+            return null;
+        }
+        root.left = RemoveLeafNodes(root.left);
+        root.right = RemoveLeafNodes(root.right);
+        return root;
+    }
+    static boolean isbal;
+    public static int isBal(BinaryTreeNode<Integer> root) //a tree is said to be balanced when difference between its left subtree height and right subtree height is not greater than 1 
+    {
+        if(root == null)
+        {
+            return 0;
+        }
+        int lh = isBal(root.left);
+        int rh = isBal(root.right);
+        int height = Math.max(lh, rh) + 1;
+        isbal = Math.abs(lh - rh) <= 1; //Math.abs keeps answer in positive integer if gap between left height and right height is greater than 1 isbal will store false in it  
+        return height;
+    }
+    public static BalPair isBal2(BinaryTreeNode<Integer> root)  
+    {
+        if(root == null)
+        {
+            BalPair bp = new BalPair();
+            bp.height = 0;
+            bp.bal = true;
+            return bp;
+        }
+        BalPair lh = isBal2(root.left);
+        BalPair rh = isBal2(root.right);
+        int TotalHeight = Math.max(lh.height, rh.height) + 1; //we have to set height also as every parent node needs subtree height to check whether its right and left is balanced
+        boolean isbal = lh.height - rh.height <= 1 && lh.bal && rh.bal;  //current node is balanced its left subtree is balanced and its right subtree is balanced if all are true then isbal will store true
+        BalPair bp = new BalPair();
+        bp.bal = isbal;
+        bp.height = TotalHeight;
+        return bp;
+
+    }
+
     public static void main(String[] args) {
-    //   BinaryTreeNode<Integer> root = LevelWiseInput();
-    //   print(root);
-    //   pair<Integer,Integer> output = heightdiameter(root);
-    //   System.out.println("Diameter is "+output.second);
-    //   System.out.println("height is "+output.first);
-    //   System.out.println("inorder");
-    //   inorder(root);
-    //   System.out.println("postorder");
-    //   postorder(root);
-    //   System.out.println("preorder");
-    //   preorder(root);
+      BinaryTreeNode<Integer> root = LevelWiseInput();
+      print(root);
+      pair<Integer,Integer> output = heightdiameter(root);
+      System.out.println("Diameter is "+output.second);
+      System.out.println("height is "+output.first);
+      System.out.println("inorder");
+      inorder(root);
+      System.out.println("postorder");
+      postorder(root);
+      System.out.println("preorder");
+      preorder(root);
     // int in[] = {4,2,5,1,6,3,7};
     // int pre[] = {1,2,4,5,3,6,7};
     // BinaryTreeNode<Integer> root = buildtreehelper(in, pre);
     // print(root);
-    int in1[] = {4,2,5,1,6,3,7};
-    int po[] = { 4,5,2,6,7,3,1};
-    BinaryTreeNode<Integer> root = BuildTreeFromPostOrder(in1, po);
+    // int in1[] = {4,2,5,1,6,3,7};
+    // int po[] = { 4,5,2,6,7,3,1};
+    // BinaryTreeNode<Integer> root = BuildTreeFromPostOrder(in1, po);
     print(root);
-    // System.out.println(IsPresent(root, 4));
-
+    System.out.println(IsPresent(root, 4));
+    System.out.println(SumofNodes(root));
+    RemoveLeafNodes(root);
+    print(root);
     }
 }
