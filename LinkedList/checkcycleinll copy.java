6f33fa798c65d3bfc3139723d2dd0c4c8e1b55dc
Umang Behl <umangbehl2002@gmail.com>
2022-07-17 23:30:36 +0530

    Check cycle in a LinkedList

---
+++ LinkedList/checkcycleinll copy.java
@@ -0,0 +1,17 @@
+package Linkedlist;
+
+class checkcycleinll {
+    public boolean hasCycle(Node<Integer> head) {
+        Node<Integer> slow = head;
+        Node<Integer> fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
