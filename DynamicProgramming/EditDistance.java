74a5220fcec1fd3698ee93390142938fff760b4a
Umang Behl <umangbehl2002@gmail.com>
2022-07-31 21:56:04 +0530

    Edit distance recursion dp

--- DynamicProgramming/EditDistance.java
+++ DynamicProgramming/EditDistance.java
@@ -27,25 +27,26 @@ public class EditDistance
   {
       int m = s1.length();
       int n = s2.length();
-      int dp[][] = new int[m+1][n+1]; //for m and n length of string there are m+1 * n + 1 unique calls and result of the string come on the dp[m][n] index if any of the length of the string is 0 then we assign the length of the second string on that index which is our base case 
-      for(int j = 0;j<dp[0].length;j++) 
+      int dp[][] = new int[m+1][n+1]; //for m and n length of string there are m+1 * n + 1 unique calls i.e for 3,3 both string length 16 blocks of matrix or calle are filled and result of the string come on the dp[m][n] index if any of the length of the string is 0 then we assign the length of the second string on that index which is our base case 
+      for(int j = 0;j<=n;j++)  //if length of m or s1 is 0 then to make it equal to s2 whatever will be the length of s2 that will be the number of operation to be done on s1 
       {
           dp[0][j] = j;
       }
-      for(int i = 0;i<dp.length;i++)
+                              //<= m because dp array is of dp[m+1][n+1] if length of string s1 and s2 is 3,3 dp array will be of 4,4 which will have index from 0 to 3 so we wil fill the array till 3,3 which is the length of strings 
+      for(int i = 0;i<=m;i++) //if length of n or s2 is 0 then to make it equal to s1 whatever will be the length of s1 that will be the number of operation to be done on s2 
       {
          dp[i][0] = i;
       }
-      for(int i = 1;i < dp.length;i++)
+      for(int i = 1;i <= m;i++)
       {
-        for(int j = 1;j< dp[0].length;j++)
+        for(int j = 1;j<= n;j++)
         {
-            if(s1.charAt(m-i) == s2.charAt(n-j))
+            if(s1.charAt(i-1) == s2.charAt(j-1))  //first character matched when we are on the index 1,1 0th character of strings are being compared 
             {
                 dp[i][j] = dp[i-1][j-1];
             }
             else 
-            {
+            {                           //replace   //delete            //insert 
                 dp[i][j] = 1 + Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
             }
         }
@@ -54,8 +55,8 @@ public class EditDistance
 
   }
   public static void main(String[] args) {
-    String s1 = "abg";
-    String s2 = "edf";
+    String s1 = "efg";
+    String s2 = "ade";
     System.out.println(EditDistanceDp(s1, s2));
     System.out.println(EditDistanceRecure(s1,s2));
   }
