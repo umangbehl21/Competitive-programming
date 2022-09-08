2885e88c819161fd7fd1f9f530eb6609c71aa8d4
Umang Behl <umangbehl2002@gmail.com>
2022-09-08 12:43:57 +0530

    bfs 0-1 Matrix

--- Graphs/LevelTwo/ZeroOneMatrix.java
+++ Graphs/LevelTwo/ZeroOneMatrix.java
@@ -1,53 +1,56 @@
 package Graphs.LevelTwo;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ZeroOneMatrix {  //tell distance of 1 from nearest zero 
-     
+   static class pair
+   {
+      int x = 0;
+      int y = 0;
+      pair(int x,int y)
+      {
+         this.x = x;  //row index
+         this.y = y;  //column index 
+      }
+   }                      //up , right , down , left 
+   static int dirs[][] = {{-1,0},{0,1},{1,0},{0,-1}};
    public static void FindOne(int grid[][])
    {
+         Queue<pair> pendingnodes = new LinkedList<>();
          for(int i = 0;i<grid.length;i++)
          {
             for(int j = 0;j<grid[0].length;j++)
             {
-                if(grid[i][j] == 1)
+                if(grid[i][j] == 0)  //if element is 0 then add in the queue as it will help in finding the distance of the numbers that are 1 distance away 
                 {
-                    CalculateDistance(grid,i,j);
+                    pendingnodes.add(new pair(i, j)); //adding the index where we found 0
+                }
+                else 
+                {
+                    grid[i][j] = -1;  //else if number is 1 mark it unvisited 
                 }
             }
          }
+         while(!pendingnodes.isEmpty())
+         {
+            pair front = pendingnodes.remove();
+            for(int i = 0;i<dirs.length;i++)
+            {
+                    int RowDash = front.x + dirs[i][0];
+                    int ColDash = front.y + dirs[i][1];
+                    if(RowDash < grid.length && ColDash < grid[0].length && RowDash >= 0 && ColDash >= 0 && grid[RowDash][ColDash] == -1)
+                    {
+                            grid[RowDash][ColDash] = grid[front.x][front.y] + 1;  //if we find -1 on left or right or up or down then that index is unvisited then its distance will be distance of the current index from 0 + 1 
+                            pendingnodes.add(new pair(RowDash, ColDash)); //add the newly calculated distance of index element in the queue so it can be used to calculate distance of next level example if we find -1 at the distance of 1 from 0 then distance of 0 + 1 will be the distance of 1 and if we find -1 at the distance of 2 from the 0 then there will be 1 in between which willl have distance of 1 from 0 so the distance of 1 will be calculated by 1 + 1 = 2
+                    }
+
+         }
    }
-   
-   private static void CalculateDistance(int[][] grid, int i, int j) 
-   {   int min1 = 0;
-       int min2 = 0;
-       if(i-1 >= 0 && i+1 < grid.length)  //if up and down direction is within bounds then min1 can be taken 
-       {
-           min1 = Math.min(grid[i-1][j], grid[i+1][j]);
-       }
-       else if(i - 1 >= 0)  //but if up or down any of the two is not in the bounds then simply give value to min1 of that index which is in bounds it can be up or down 
-       {
-            min1 = grid[i-1][j];
-       }
-       else if(i+1 < grid.length)
-       {
-          min1 = grid[i+1][j];
-       }
-       if(j+1 < grid[0].length && j-1 >= 0) //if left and right direction is in bounds min2 can be taken 
-       {
-          min2 = Math.min(grid[i][j-1],grid[i][j+1]); 
-       }
-       else if(j - 1 >= 0) //but if any of the left or right is not in bounds assign value to min2 of the index which is in bounds it can be left or right 
-       {
-        min2 = grid[i][j-1];
-       }
-       else if(j+1 < grid[0].length)
-       {
-        min2 = grid[i][j+1];
-       }                                        
-       grid[i][j] = Math.min(min1, min2) + 1;  //take min from all the four direction if there is 0 in any of the four directions than the nearest zero will be that 0 + 1 if there is no 0 then min will be 1 so distance will be min + 1 = 1 + 1 = 2
-   }
+}
 
 public static void main(String[] args) {
-      int grid[][] = {{0,0,0},{0,0,1},{1,1,1}};
+      int grid[][] = {{0,1,1},{1,1,1},{1,1,1}};
       FindOne(grid);
       for(int i = 0;i<grid.length;i++)
       {
