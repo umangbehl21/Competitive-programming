04c353855059a249511358314082084cf853622f
Umang Behl <umangbehl2002@gmail.com>
2022-09-30 17:27:36 +0530

    BellMan Ford Algo

--- Graphs/LevelTwo/BellManFordAlgorithm.java
+++ Graphs/LevelTwo/BellManFordAlgorithm.java
@@ -2,7 +2,7 @@ package Graphs.LevelTwo;
 import java.util.*;
 public class BellManFordAlgorithm   //bellman ford works on the negative edges where the djisktra donnot give the shortest path for negative edges and also bellman the Bellman-Ford algorithm can handle directed and undirected graphs with non-negative weights. However, it can only handle directed graphs with negative weights, as long as we don't have negative cycles.
 {
-    public static void calculateShortestDistanceFromSourceToEveryNode(int arr[][],int V) //v is total vertices
+    public static void calculateShortestDistanceFromSourceToEveryNode(int arr[][],int V,int edges) //v is total vertices
     {
         int path[] = new int[V];
         for(int i = 1;i<path.length;i++)
@@ -11,7 +11,7 @@ public class BellManFordAlgorithm   //bellman ford works on the negative edges w
         }
         for(int i = 1;i<=V - 1;i++) //outer loop starts from i = 1 as for i = 0 or 0th node distance is zero from itself the ith iteration runs for every vertice and try to find the shortest distance for example for i = 1 j will traverse on whole array but the node which is closest to the src or 0th node its distance can be calculated for i = 1th iteration but it will try to find the shortest path for every element in every iteration 
         {
-            for(int j = 0;j<path.length;j++) //inner loop runs on the path array it checks if the path[src] + distance between src and neighbour < path[nbr] if path of source plus the distance between src and neighbour is less than the previously initialised path of the neighbour then update the distance of neighbour 
+            for(int j = 0;j<edges;j++) //inner loop runs for every edge and moves to every possible edges to find the shortest path in weight from source to other nodes it checks if the path[src] + distance between src and neighbour < path[nbr] if path of source plus the distance between src and neighbour is less than the previously initialised path of the neighbour then update the distance of neighbour 
             {
                 int src = arr[j][0];
                 int nbr = arr[j][1];
@@ -28,7 +28,7 @@ public class BellManFordAlgorithm   //bellman ford works on the negative edges w
             }
 
         }
-        for(int j = 0;j<path.length;j++) //inner loop runs on the path array it checks if the path[src] + distance between src and neighbour < path[nbr] if path of source plus the distance between src and neighbour is less than the previously initialised path of the neighbour then update the distance of neighbour 
+        for(int j = 0;j<edges;j++) //inner loop runs on the path array it checks if the path[src] + distance between src and neighbour < path[nbr] if path of source plus the distance between src and neighbour is less than the previously initialised path of the neighbour then update the distance of neighbour 
         {
             int src = arr[j][0];
             int nbr = arr[j][1];
@@ -76,6 +76,6 @@ public class BellManFordAlgorithm   //bellman ford works on the negative edges w
             arr[i][1] = nbr;
             arr[i][2] = wt;
         }
-        calculateShortestDistanceFromSourceToEveryNode(arr, vertices);
+        calculateShortestDistanceFromSourceToEveryNode(arr, vertices,edges);
     }
 }
